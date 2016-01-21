fcd6dff1b8cbe3c4a111fbd63fa2eaf781617704
haoli <haoli@marvell.com>
2016-01-21 15:31:12 +0800

    Just some polish to make it work as expected

--- src/draw_a_pickture.c
+++ src/draw_a_pickture.c
@@ -1,13 +1,13 @@
 /********************************************************************************/
 /* This framework is from                                                       */
 /* http://codegolf.stackexchange.com/questions/35569/tweetable-mathematical-art */
-/* And I've just copy the framework and a sample for furether play              */
+/* And I've just copy the framework and a sample for furether play
+/* Just remember to comple it with -lm option to make the match function take effect */
 /* Have fun!!!!                                                                 */
 /********************************************************************************/
 
-
 #include <math.h>
-//#include <cstdlib>
+#include <tgmath.h>
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -17,12 +17,10 @@
 #define _cb(x) abs((x)*(x)*(x)) // absolute value of cube
 #define _cr(x) (unsigned char)(pow((x),1.0/3.0)) // cube root
 
-#define SWITCH MARTIN
+#define MARTIN
+#define MAX_PRINT 20
 
-#if (defined(SWITCH) && (SWITCH == VOID))
-unsigned char GR(int,int);
-unsigned char BL(int,int);
-
+#if defined VOID
 unsigned char RD(int i,int j){
    // YOUR CODE HERE
     return 0;
@@ -35,18 +33,19 @@ unsigned char BL(int i,int j){
    // YOUR CODE HERE
     return 0;
 }
+#endif
 
-#elif (defined(SWITCH) && (SWITCH == MARTIN))
+#if defined MARTIN
 unsigned char RD(int i,int j){
-return (char)(_sq(cos(atan2(j-512,i-512)/2))*255);
+    return (char)(_sq(cos(atan2(j-512,i-512)/2))*255);
 }
 
 unsigned char GR(int i,int j){
-return (char)(_sq(cos(atan2(j-512,i-512)/2-2*acos(-1)/3))*255);
+    return (char)(_sq(cos(atan2(j-512,i-512)/2-2*acos(-1)/3))*255);
 }
 
 unsigned char BL(int i,int j){
-return (char)(_sq(cos(atan2(j-512,i-512)/2+2*acos(-1)/3))*255);
+    return (char)(_sq(cos(atan2(j-512,i-512)/2+2*acos(-1)/3))*255);
 }
 #endif
 
