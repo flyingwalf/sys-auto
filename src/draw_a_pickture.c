385bdbe66820acb093e2fb1d7b4be0f019641177
haoli <haoli@marvell.com>
2016-01-21 14:44:50 +0800

    just get something fun, have it here, see commit details

---
+++ src/draw_a_pickture.c
@@ -0,0 +1,72 @@
+/********************************************************************************/
+/* This framework is from                                                       */
+/* http://codegolf.stackexchange.com/questions/35569/tweetable-mathematical-art */
+/* And I've just copy the framework and a sample for furether play              */
+/* Have fun!!!!                                                                 */
+/********************************************************************************/
+
+
+#include <math.h>
+//#include <cstdlib>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define DIM 1024
+#define DM1 (DIM-1)
+#define _sq(x) ((x)*(x)) // square
+#define _cb(x) abs((x)*(x)*(x)) // absolute value of cube
+#define _cr(x) (unsigned char)(pow((x),1.0/3.0)) // cube root
+
+#define SWITCH MARTIN
+
+#if (defined(SWITCH) && (SWITCH == VOID))
+unsigned char GR(int,int);
+unsigned char BL(int,int);
+
+unsigned char RD(int i,int j){
+   // YOUR CODE HERE
+    return 0;
+}
+unsigned char GR(int i,int j){
+   // YOUR CODE HERE
+    return 0;
+}
+unsigned char BL(int i,int j){
+   // YOUR CODE HERE
+    return 0;
+}
+
+#elif (defined(SWITCH) && (SWITCH == MARTIN))
+unsigned char RD(int i,int j){
+return (char)(_sq(cos(atan2(j-512,i-512)/2))*255);
+}
+
+unsigned char GR(int i,int j){
+return (char)(_sq(cos(atan2(j-512,i-512)/2-2*acos(-1)/3))*255);
+}
+
+unsigned char BL(int i,int j){
+return (char)(_sq(cos(atan2(j-512,i-512)/2+2*acos(-1)/3))*255);
+}
+#endif
+
+void pixel_write(int,int);
+FILE *fp;
+int main(){
+    fp = fopen("MathPic.ppm","wb");
+    fprintf(fp, "P6\n%d %d\n255\n", DIM, DIM);
+    int i, j;
+    for(j=0;j<DIM;j++)
+        for(i=0;i<DIM;i++)
+            pixel_write(i,j);
+    fclose(fp);
+    return 0;
+}
+
+void pixel_write(int i, int j){
+    static unsigned char color[3];
+    color[0] = RD(i,j)&255;
+    color[1] = GR(i,j)&255;
+    color[2] = BL(i,j)&255;
+    fwrite(color, 1, 3, fp);
+}
